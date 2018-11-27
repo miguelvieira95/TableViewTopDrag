5a4d89afea37db3df13cb962fc666d29550c37fc
Miguel Vieira <miguelvieira11@gmail.com>
2018-11-27 10:28:39 +0000

    Initial Commit

---
+++ TableViewTopDrag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTopDrag
+//
+//  Created by Miguel Vieira on 27/11/2018.
+//  Copyright © 2018 Miguel Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
